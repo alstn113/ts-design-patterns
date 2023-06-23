c5a341e12cc9055bda886ac39f65a004301c2801
김민수 MINSOO KIM <alstn113@gmail.com>
2023-06-24 00:20:59 +0900

    feat: factory pattern

--- Design-Patterns/Creational-Design-Pattern/Constructor/index.ts
+++ Design-Patterns/Creational-Design-Pattern/Constructor/index.ts
@@ -1,14 +1,22 @@
-class NewPerson {
-  constructor(private name: string, private age: number) {
+class Product {
+  private name: string;
+  private price: number;
+
+  constructor(name: string, price: number) {
     this.name = name;
-    this.age = age;
+    this.price = price;
   }
 
-  public say() {
-    console.log(`My name is ${this.name}, I'm ${this.age} years old.`);
+  public getName(): string {
+    return this.name;
+  }
+
+  public getPrice(): number {
+    return this.price;
   }
 }
 
-const newPerson = new NewPerson('John', 20);
-
-newPerson.say(); // My name is John, I'm 20 years old.
+// 객체 생성과 초기화
+const product = new Product('Example Product', 10);
+console.log(product.getName()); // "Example Product"
+console.log(product.getPrice()); // 10
---
+++ Design-Patterns/Creational-Design-Pattern/Factory/index.ts
@@ -0,0 +1,57 @@
+// 인터페이스: 생성될 도형 객체가 구현해야 할 메서드를 정의합니다.
+interface Shape {
+  draw(): void;
+}
+
+// 구체적인 도형 클래스들
+class Circle implements Shape {
+  draw(): void {
+    console.log('원을 그립니다.');
+  }
+}
+
+class Rectangle implements Shape {
+  draw(): void {
+    console.log('사각형을 그립니다.');
+  }
+}
+
+class Triangle implements Shape {
+  draw(): void {
+    console.log('삼각형을 그립니다.');
+  }
+}
+
+// 팩토리 클래스
+class ShapeFactory {
+  // 팩토리 메서드: 클라이언트에게 요청된 도형 객체를 생성하여 반환합니다.
+  public createShape(type: string): Shape {
+    if (type === 'Circle') {
+      return new Circle();
+    } else if (type === 'Rectangle') {
+      return new Rectangle();
+    } else if (type === 'Triangle') {
+      return new Triangle();
+    } else {
+      throw new Error('지원되지 않는 도형입니다.');
+    }
+  }
+}
+
+// 클라이언트 코드
+const factory = new ShapeFactory();
+
+const circle = factory.createShape('Circle');
+circle.draw(); // 원을 그립니다.
+
+const rectangle = factory.createShape('Rectangle');
+rectangle.draw(); // 사각형을 그립니다.
+
+const triangle = factory.createShape('Triangle');
+triangle.draw(); // 삼각형을 그립니다.
+
+try {
+  factory.createShape('Square');
+} catch (e: any) {
+  console.log(e.message); // 지원되지 않는 도형입니다.
+}
